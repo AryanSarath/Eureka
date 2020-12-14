a2af24778cc615938dd54413482bb93eb3ad153a
AryanSarath <71126951+AryanSarath@users.noreply.github.com>
2020-12-14 14:11:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Eureka
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ TopBar.js
@@ -0,0 +1,18 @@
+class TopBar {
+    constructor(){
+        var options = {
+            isStatic : true
+        }
+
+        this.body = Bodies.rectangle (400, 100, 200, 25, options);
+        this.width = 200;
+        this.height = 25;
+        World .add (world, this.body);
+    }
+
+    display (){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        rect (pos.x, pos.y, this.width, this.height);
+    }
+}
